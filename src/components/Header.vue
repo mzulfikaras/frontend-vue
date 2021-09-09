ee4de79d47dbe62406cc75b1256ed3b51ae78259
mzulfikaras <zulfikaralisunan96@gmail.com>
2021-09-09 17:33:21 +0700

    update 2

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,7 +6,7 @@
                     <div class="row align-items-center">
                         <div class="col-md-3 col-7">
                             <router-link :to="{name: 'home'}" class="text-decoration-none" data-abc="true">
-                                <span class="logo"><i class="fa fa-apple-alt"></i>APPLE STORE</span>
+                                <span class="logo"><i class="fa fa-apple-alt"></i>SEMUA STORE</span>
                             </router-link>
                         </div>
                         <div class="col-md-5 d-none d-md-block">
